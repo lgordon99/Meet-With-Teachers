5a67882c5f75e548973b0b1f719da4c1a7920c88
lgordon99 <luciabeluga1@gmail.com>
2016-08-12 01:18:51 -0400

    Colorful sliders and updateText method

--- Meet With Teachers/ViewController.swift
+++ Meet With Teachers/ViewController.swift
@@ -21,6 +21,17 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     var teachers: [String] = ["Ms. Lubman", "Mr. Lindow", "Ms. Spalding", "Ms. McCutcheon", "Mr. McClintock", "Mr. Tarbath", "Ms. LiCalsi", "Doctor Henry", "Mr. Raisher"]
     var day = "A"
     var period = "1"
+    var time = "A1"
+    var teacher = "Ms. Lubman"
+    let lubman: [String] = ["A2", "A4", "A5", "A7", "B1", "B5", "B7", "C4", "D1", "D2", "D4", "D5", "D7", "E1", "E2", "E5", "F2", "F7", "G1", "G2", "G5", "G6", "H2", "H5", "I1", "I5", "I7", "J4", "J7"]
+    let lindow: [String] = ["A6", "A7", "B1", "B2", "B4", "C2", "C4", "C6", "D6", "D7", "E6", "E7", "F1", "F2", "F4", "F5", "G5", "G6", "G7", "H1", "H7", "I1", "I2", "I4", "J2", "J4", "J6"]
+    let spalding: [String] = ["A1", "A7", "B1", "B2", "B4", "B6", "C4", "C5", "C6", "C7", "D1", "D7", "E4", "E6", "E7", "F2", "F4", "F5", "F6", "F7", "G6", "G7", "H1", "H4", "I1", "I2", "I4", "I6", "J4", "J5", "J6", "J7"]
+    let mcCutcheon: [String] = ["A6", "A7", "B1", "B2", "B4", "C2", "C4", "C5", "C6", "C7", "D1", "D6", "D7", "E6", "E7", "F1", "F2", "F4", "F5", "F7", "G5", "G6", "G7", "H1", "H7", "I1", "I2", "I4", "J2", "J4", "J5", "J6", "J7"]
+    let mcClintock: [String] = ["A2", "A5", "A6", "A7", "B2", "B4", "B7", "C2", "C4", "C6", "D1", "D2", "D5", "D6", "E2", "E7", "F1", "F2", "F4", "F5", "F7", "G4", "G5", "H1", "H2", "H5", "H7", "I1", "I2", "I4", "J1", "J2", "J6"]
+    let tarbath: [String] = ["A1", "A2", "A5", "A7", "B2", "B5", "B6", "C1", "C5", "C7", "D1", "D2", "D5", "E1", "E4", "E5", "E6", "F4", "F5", "F6", "F7", "G1", "G4", "G7", "H2", "H4", "H6", "I2", "I5", "I6", "J1", "J5", "J7"]
+    let liCalsi: [String] = ["A1", "A2", "A4", "A5", "A6", "B5", "B6", "B7", "C1", "C2", "C5", "C6", "C7", "D1", "D2", "D4", "D5", "D6", "E1", "E2", "E4", "E5", "F1", "F4", "F5", "F6", "F7", "G1", "G2", "G4", "G5", "H2", "H4", "H5", "H6", "H7", "I4", "I5", "I6", "I7", "J1", "J2", "J5", "J7"]
+    let henry: [String] = ["A4", "A6", "B1", "B4", "B5", "C1", "C2", "C6", "C7", "D4", "D6", "D7", "E1", "E5", "E6", "E7", "F1", "F5", "G1", "G2", "G5", "G6", "G7", "H1", "H6", "H7", "I4", "I5", "I7", "J2", "J4", "J5", "J6"]
+    let raisher: [String] = ["A4", "A5", "A7", "B1", "B4", "B6", "C1", "C5", "D4", "D5", "D6", "D7", "E4", "E5", "E6", "F2", "F5", "F7", "G2", "G4", "G5", "G6", "G7", "H4", "H5", "H6", "I4", "I7", "J1", "J2", "J4"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,12 +62,14 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         let numbersToLetters: [Int: String] = [1: "A", 2: "B", 3: "C", 4: "D", 5: "E", 6: "F", 7: "G", 8: "H", 9: "I", 10: "J"]
         dayLabel.text = "Day: \(numbersToLetters[currentValue]!)"
         day = numbersToLetters[currentValue]!
+        updateText()
     }
     
     @IBAction func periodSliderChanged(sender: AnyObject) {
         let currentValue = Int(periodSlider.value)
         periodLabel.text = "Period: \(currentValue)"
         period = String(currentValue)
+        updateText()
     }
     
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
@@ -78,20 +91,12 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         availabilityLabel.hidden = false
         availabilityLabel.text = "Availability"
-        
-        let lubman: [String] = ["A2", "A4", "A5", "A7", "B1", "B5", "B7", "C4", "D1", "D2", "D4", "D5", "D7", "E1", "E2", "E5", "F2", "F7", "G1", "G2", "G5", "G6", "H2", "H5", "I1", "I5", "I7", "J4", "J7"]
-        let lindow: [String] = ["A6", "A7", "B1", "B2", "B4", "C2", "C4", "C6", "D6", "D7", "E6", "E7", "F1", "F2", "F4", "F5", "G5", "G6", "G7", "H1", "H7", "I1", "I2", "I4", "J2", "J4", "J6"]
-        let spalding: [String] = ["A1", "A7", "B1", "B2", "B4", "B6", "C4", "C5", "C6", "C7", "D1", "D7", "E4", "E6", "E7", "F2", "F4", "F5", "F6", "F7", "G6", "G7", "H1", "H4", "I1", "I2", "I4", "I6", "J4", "J5", "J6", "J7"]
-        let mcCutcheon: [String] = ["A6", "A7", "B1", "B2", "B4", "C2", "C4", "C5", "C6", "C7", "D1", "D6", "D7", "E6", "E7", "F1", "F2", "F4", "F5", "F7", "G5", "G6", "G7", "H1", "H7", "I1", "I2", "I4", "J2", "J4", "J5", "J6", "J7"]
-        let mcClintock: [String] = ["A2", "A5", "A6", "A7", "B2", "B4", "B7", "C2", "C4", "C6", "D1", "D2", "D5", "D6", "E2", "E7", "F1", "F2", "F4", "F5", "F7", "G4", "G5", "H1", "H2", "H5", "H7", "I1", "I2", "I4", "J1", "J2", "J6"]
-        let tarbath: [String] = ["A1", "A2", "A5", "A7", "B2", "B5", "B6", "C1", "C5", "C7", "D1", "D2", "D5", "E1", "E4", "E5", "E6", "F4", "F5", "F6", "F7", "G1", "G4", "G7", "H2", "H4", "H6", "I2", "I5", "I6", "J1", "J5", "J7"]
-        let liCalsi: [String] = ["A1", "A2", "A4", "A5", "A6", "B5", "B6", "B7", "C1", "C2", "C5", "C6", "C7", "D1", "D2", "D4", "D5", "D6", "E1", "E2", "E4", "E5", "F1", "F4", "F5", "F6", "F7", "G1", "G2", "G4", "G5", "H2", "H4", "H5", "H6", "H7", "I4", "I5", "I6", "I7", "J1", "J2", "J5", "J7"]
-        let henry: [String] = ["A4", "A6", "B1", "B4", "B5", "C1", "C2", "C6", "C7", "D4", "D6", "D7", "E1", "E5", "E6", "E7", "F1", "F5", "G1", "G2", "G5", "G6", "G7", "H1", "H6", "H7", "I4", "I5", "I7", "J2", "J4", "J5", "J6"]
-        let raisher: [String] = ["A4", "A5", "A7", "B1", "B4", "B6", "C1", "C5", "D4", "D5", "D6", "D7", "E4", "E5", "E6", "F2", "F5", "F7", "G2", "G4", "G5", "G6", "G7", "H4", "H5", "H6", "I4", "I7", "J1", "J2", "J4"]
-        
-        let time = day + period
-        let teacher = teachers[row]
-        
+        time = day + period
+        teacher = teachers[row]
+        updateText()
+    }
+    
+    func updateText () {
         if teacher == "Ms. Lubman" {
             for item in lubman {
                 if time == item {
